fbdc17d1eb33b11eb2e96b64446062984e76a221
Vitor Hugo <vitor.toniasso@outlook.com>
2023-10-25 12:20:38 -0300

    Desafios

---
+++ Desafio Pokedex/script/script.js
@@ -0,0 +1,9 @@
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+
+    if (APIResponse.status === 200) 
+      const data = await APIResponse.json();
+      return data;
+    }
+
+    fetchPokemon('25');
